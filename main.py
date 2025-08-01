c43af60955f4dd6cf4c602dcd78d4be5972b2733
pro6836 <aurtyytt@gmail.com>
2025-08-01 14:14:43 -0400

    Added main.py and todo's

---
+++ main.py
@@ -0,0 +1,18 @@
+#IA-Geyser BASIC SERVER
+# -- ONLY FOR 1 SERVER
+# -- FOR MORE SERVER, USE IA-Geyser-Server-Queue for a queue based system
+# Version 3.0
+# GPL-3.0 license
+# Revision Date: 01/08/2025
+# Description: This is the basic server for IA-Geyser
+# -- It will handle the connection to the client and the server
+# -- It will handle the download of the pack
+# -- It will handle the conversion of the pack ( Items )
+# -- It will handle the temporary storage of the pack
+# -- It will handle the sending of the pack download url to the plugin
+
+
+#TODO: Implement WSS connection
+#TODO: Implement Download Pack
+#TODO: Implement Kas-tle's Converter
+#TODO: Implement Block/Entity Conversion
